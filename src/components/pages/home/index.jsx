e9188720b1ea67165007dbd31e8fb38f42404207
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-08-15 07:31:37 -0300

    iniciando a componentização

---
+++ src/components/pages/home/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { HomeContainer } from './styled';
+
+function Home() {
+  return (
+    <HomeContainer>
+        <div>
+        Oiiiiiiii
+        <hr/>
+        vai dar bomm
+          </div>
+    </HomeContainer>
+  );
+}
+
+export default Home;
